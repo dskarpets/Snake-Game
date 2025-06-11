667ea132bdf30a3153ccc48548382ee91d109c6f
daniilkarpets <daniil.karpets20@gmail.com>
2025-06-11 16:18:40 +0300

    Improved project structure

---
+++ app/main.py
@@ -0,0 +1,54 @@
+import time
+from tkinter.font import names
+from turtle import Screen, Turtle
+from app.scoreboard import Scoreboard
+from app.snake import Snake
+from app.food import Food
+
+def main():
+    screen = Screen()
+    screen.setup(height=600, width=600)
+    screen.bgcolor("black")
+    screen.title("Snake game")
+    screen.tracer(0)
+
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+
+    screen.listen()
+    screen.onkey(key="Up", fun=snake.up)
+    screen.onkey(key="Down", fun=snake.down)
+    screen.onkey(key="Left", fun=snake.left)
+    screen.onkey(key="Right", fun=snake.right)
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+
+        snake.move()
+
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        # Detect collision with wall
+        x, y = snake.head.xcor(), snake.head.ycor()
+        if abs(x) > 290 or y < -290 or y > 250:
+            game_is_on = False
+            scoreboard.game_over()
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
